a1d4a434298f2d9bc54f281f1ebbb20c4ce1b4c2
Pratham_Mishra04 <prathamm0409@gmail.com>
2023-06-03 01:24:19 +0530

    feat: minor changes in hero section

--- src/sections/landing-page-sections/heroSection.tsx
+++ src/sections/landing-page-sections/heroSection.tsx
@@ -6,11 +6,11 @@ const HeroSection = () => {
   return (
     <>
       <div className="h-[90vh] flex justify-around items-center">
-        <div className="h-full w-[50%] font-Poppins px-12 flex gap-5 flex-col justify-center items-center">
-          <p className="text-9xl w-full font-bold">FERN.</p>
-          <p className="text-3xl w-full">- Investment Analysis</p>
+        <div className="h-full w-1/2 font-Poppins px-12 flex gap-5 flex-col justify-center items-center">
+          <p className="text-9xl font-bold">FERN.</p>
+          <p className="text-3xl mb-24">- Investment Analysis</p>
         </div>
-        <div className="h-full w-[50%] flex justify-around items-center">
+        <div className="h-full w-1/2 flex justify-around items-center">
           <Model />
         </div>
       </div>
