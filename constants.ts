f14ffb1fa139e3a7a03b91df737ed72499a9a589
Pratham_Mishra04 <prathamm0409@gmail.com>
2023-06-02 19:49:10 +0530

    feat: project setup

---
+++ constants.ts
@@ -0,0 +1,9 @@
+export const DEV_BACKEND_URL = "http://localhost:8000"
+
+export const DEV_USER_PROFILE_PIC_URL = "http://localhost:8000/users/profilePics"
+
+export const DEV_PROJECT_PIC_URL = "http://localhost:8000"
+
+export const DEV_POST_PIC_URL = "http://localhost:8000"
+
+export const COMMENT_PER_PAGE_LIMIT=2;
